3f76ca08d8644bfe22e3be97c1a25223f7ce77cf
StanleyYuGalan <yg_stan@yahoo.com>
2015-11-27 21:58:25 +0800

    Added Music
    Added music to the game stolen from fallout 4

--- Tank Game/Assets/Resources/Scripts/GUIScript.cs
+++ Tank Game/Assets/Resources/Scripts/GUIScript.cs
@@ -5,21 +5,23 @@ public class GUIScript : MonoBehaviour {
 	public Rect windowRect;
 	public string dataBox1Content = "Data Box 1";
 	public string dataBox2Content = "Data Box 2";
+	public AudioClip music;
 	Controls2 enemyControls;
 	GameObject enemy;
 	// Use this for initialization
 	void Start () {
+		music = Resources.Load<AudioClip>("Audio/music"); 
 		enemy = GameObject.Find ("Player 2");
 		enemyControls = (Controls2)enemy.GetComponent("Controls2");
 		windowRect = new Rect(450, 150, 250, 250);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (enemyControls == null)
-		//print (enemyControls.hp);
-			print ("victory");
+if (music == null) {
+			print("music does not work");
+		}
 		}
 	
 	public void OnGUI() {
