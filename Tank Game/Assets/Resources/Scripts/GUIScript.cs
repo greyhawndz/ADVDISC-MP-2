22c58540adb7cea246024a8f00cc4b0aa1eb8e3a
StanleyYuGalan <yg_stan@yahoo.com>
2015-11-27 17:06:20 +0800

    Matrix Formula Boxes Added
    Matrix Formula Boxes Added

---
+++ Tank Game/Assets/Resources/Scripts/GUIScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour {
+	public Rect windowRect;
+	public string dataBox1Content = "Data Box 1";
+	public string dataBox2Content = "Data Box 2";
+	// Use this for initialization
+	void Start () {
+		windowRect = new Rect(450, 150, 250, 250);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	public void OnGUI() {
+		
+		GUI.Box (new Rect (5, 405, 250, 185), dataBox1Content);
+
+		GUI.Box (new Rect (805, 5, 250, 185), dataBox2Content);
+	
+	}
+}
